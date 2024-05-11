1a78e5799c89f92e3f6d2a84b7340dbecd3e7175
towhid046 <towhidmorol46@gmail.com>
2024-05-11 15:15:43 +0600

    added search functionality to all services page

--- src/pages/AllServicesPage/AllServicesPage.jsx
+++ src/pages/AllServicesPage/AllServicesPage.jsx
@@ -1,34 +1,61 @@
-import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import SingleService from "../../components/unique/SingleService/SingleService";
 import SectionHeader from "../../components/shared/SectionHeader/SectionHeader";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { scrollToTop } from "../../utilities/scrollToTop";
-import DynamicHelmet from './../../components/shared/DynamicHelmet/DynamicHelmet';
+import DynamicHelmet from "./../../components/shared/DynamicHelmet/DynamicHelmet";
 
 const AllServicesPage = () => {
+  const [searchText, setSearchText] = useState("");
+  const [services, setServices] = useState([]);
+  const [isError, setIsError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchLoading, setSearchLoading] = useState(false);
+
   useEffect(() => {
     scrollToTop();
   }, []);
 
-  const {
-    data: services,
-    isLoading,
-    error,
-    isError,
-  } = useQuery({
-    queryKey: ["services"],
-    queryFn: async () => {
-      const res = await axios.get(`${import.meta.env.VITE_API_URL}/services`);
-      return res.data;
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get(
+          `${import.meta.env.VITE_API_URL}/services?search=${searchText}`
+        );
+        setServices(res.data);
+      } catch (error) {
+        setIsError(error?.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getData();
+  }, [searchText]);
 
-    },
-  });
+  const handleSearchService = (e) => {
+    e.preventDefault();
+    const value = e.target.search.value;
+    setSearchText(value);
+  };
+
+  const handleSearchOnChange = async (e) => {
+    const searchVal = e.target.value;
+    try {
+      setSearchText(searchVal);
+      setSearchLoading(true);
+    } catch (error) {
+      setIsError(error?.message);
+    } finally {
+      setSearchLoading(false);
+    }
+  };
 
   if (isError) {
     return (
       <div className="flex justify-center py-12 min-h-screen ">
-        <h2 className="text-2xl font-bold text-gray-300">{error}</h2>
+        <h2 className="text-2xl font-bold text-gray-300">
+          No Search data Have found
+        </h2>
       </div>
     );
   }
@@ -43,13 +70,51 @@ const AllServicesPage = () => {
 
   return (
     <section className="mb-12">
-      <DynamicHelmet title='Services'/>
+      <DynamicHelmet title="Services" />
       <SectionHeader />
-      <div className="flex flex-col gap-6">
-        {services?.map((service) => (
-          <SingleService key={service._id} service={service} />
-        ))}
+      <div className="my-8">
+        <form onSubmit={handleSearchService} className="w-full">
+          <label className="input input-bordered max-w-md flex mx-auto items-center gap-2">
+            <input
+              onChange={handleSearchOnChange}
+              type="text"
+              name="search"
+              className="grow"
+              placeholder="Search services"
+            />
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-5 h-5 opacity-70 cursor-pointer"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </label>
+        </form>
       </div>
+      {searchLoading ? (
+        <div className="flex justify-center py-10">
+          <span className="loading loading-spinner loading-md"></span>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-6">
+          {services?.map((service) => (
+            <SingleService key={service._id} service={service} />
+          ))}
+        </div>
+      )}
+      {services.length <= 0 && (
+        <h2 className="text-2xl font-semibold italic text-gray-300 py-10 text-center">
+          Sorry, No service have match in your search!
+        </h2>
+      )}
     </section>
   );
 };
