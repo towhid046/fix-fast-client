6521e74bd30b570f617ce98c2a39c22db12ba9d5
towhid046 <towhidmorol46@gmail.com>
2024-05-14 15:41:43 +0600

    added style to not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -3,13 +3,14 @@ import DynamicHelmet from './../../components/shared/DynamicHelmet/DynamicHelmet
 
 const NotFoundPage = () => {
   return (
-    <section className="min-h-screen flex flex-col gap-5 justify-center items-center space-y-5">
+    <section className="min-h-screen bg-gray-900 flex flex-col gap-3 justify-center items-center">
       <DynamicHelmet title="Error"/>
-      <h2 className="text-gray-300 font-semibold text-center text-3xl">
+      <h1 className="text-gray-400 font-bold xl:text-5xl lg:text-4xl text-3xl">Opps! Error: 404</h1>
+      <h2 className="text-gray-600 font-semibold text-center lg:text-3xl text-2xl">
         Page not found
       </h2>
       <Link to={"/"} className="">
-        <button className="btn btn-error">Return to Home</button>
+        <button className="btn md:btn-md btn-sm btn-error btn-outline">Return to Home</button>
       </Link>
     </section>
   );
